9ec9b2c44b1913596b9a06fe15c6443e9aba2e6b
Benjamin Verdier <bverdier@bu.edu>
2019-09-10 18:09:36 -0400

    slight optimization and viewer setup

--- main.cpp
+++ main.cpp
@@ -27,92 +27,77 @@ const std::string BUNNY = "/home/bverdier/voxelizer/data/bunny.off";
 int cli(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
-{
-  //initializing things
+{   
+    //initializing things
 
-  //parse cli args
-  return cli(argc, argv);
-  // Load a mesh in OFF format
-  Voxelizer vox;
-  vox.setRes(32);
-  vox.loadMesh(BUNNY);
-  vox.computeVoxels();
-  vox.computeMesh(V,F);
-
-  // Plot the mesh
-  opengl::glfw::Viewer viewer;
-  viewer.data().set_mesh(V, F);
-
-  // Launch the viewer
-  viewer.launch();
+    //parse cli args
+    return cli(argc, argv);
 }
 
 int cli(int argc, char *argv[]) {
-  std::string file = BUNNY;
-  std::string saveFile = "";
-  bool voxelize = false;
-  bool saveVox = false;
-  bool fileIsVoxels = false;
-  int res = 32;
+    std::string file = BUNNY;
+    std::string saveFile = "";
+    bool voxelize = false;
+    bool saveVox = false;
+    bool fileIsVoxels = false;
+    int res = 32;
 
-  int opt;
-  while ((opt = getopt(argc, argv, "f:vs:e:r:")) != -1) {
-    switch (opt) {
-      case 'f':
-        file = optarg;
-        break;
-      case 'v':
-        voxelize = true;
-        break;
-      case 's':
-        saveVox = true;
-        saveFile = optarg;
-        break;
-      case 'e':
-        fileIsVoxels = true;
-        file = optarg;
-        break;
-      case 'r':
-        if (!optarg) break;
-        res = atoi(optarg);
-        break;
-      default:
-        break;
+    int opt;
+    while ((opt = getopt(argc, argv, "f:vs:e:r:")) != -1) {
+        switch (opt) {
+            case 'f':
+                file = optarg;
+                break;
+            case 'v':
+                voxelize = true;
+                break;
+            case 's':
+                saveVox = true;
+                saveFile = optarg;
+                break;
+            case 'e':
+                fileIsVoxels = true;
+                file = optarg;
+                break;
+            case 'r':
+                if (!optarg) break;
+                res = atoi(optarg);
+                break;
+            default:
+                break;
+        }
     }
-  }
-  
-  Voxelizer vox;
-  
-  if (!fileIsVoxels) {
-    vox.loadMesh(file);
-    vox.setRes(res);
     
-    if (voxelize) {
-      vox.computeVoxels();
-      vox.computeMesh(V,F);
+    Voxelizer vox;
+    
+    if (!fileIsVoxels) {
+        vox.loadMesh(file);
+        vox.setRes(res);
+        
+        if (voxelize) {
+            vox.computeVoxels();
+            vox.computeMesh(V,F);
 
-      if (saveVox) {
-        vox.saveVoxels(saveFile);
-      }
+            if (saveVox) {
+                vox.saveVoxels(saveFile);
+            }
 
+        } else {
+            V = vox.V;
+            F = vox.F;
+        }
     } else {
-      V = vox.V;
-      F = vox.F;
+        vox.loadVoxels(file);
+        vox.computeMesh(V,F);
     }
-  } else {
-    vox.loadVoxels(file);
-    vox.computeMesh(V,F);
-  }
+    // Plot the mesh
+    opengl::glfw::Viewer viewer;
+    viewer.data().set_mesh(V, F);
+    viewer.data().set_face_based(true);
+    viewer.core().toggle(viewer.data().show_lines);
 
+    // Launch the viewer
+    viewer.launch();
 
-
-
-  // Plot the mesh
-  opengl::glfw::Viewer viewer;
-  viewer.data().set_mesh(V, F);
-
-  // Launch the viewer
-  viewer.launch();
-
-  return 0;
+    return 0;
 }
